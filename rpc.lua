73cfe31abc9870ad2fb8d112eaec7cffdca8521a
Nekzerd <79750023+Nekzerd@users.noreply.github.com>
2022-08-27 17:53:33 +0200

    Create rpc.lua

---
+++ rpc.lua
@@ -0,0 +1,21 @@
+local isRDR = not TerraingridActivate
+
+Citizen.CreateThread(function()
+	while true do
+		local playerPed = PlayerPedId()
+		local playerserver_NAME = GetPlayerName(PlayerId())
+        local playerserver_ID = GetPlayerServerId(PlayerId())
+        local playerserver_COUNT = #GetActivePlayers()
+
+		SetDiscordAppId(867767851267915787)
+        SetRichPresence(""..playerserver_NAME.. " - " ..playerserver_COUNT.. "/1024")
+
+		SetDiscordRichPresenceAsset("your logo")
+		SetDiscordRichPresenceAssetText(playerserver_NAME)
+
+        SetDiscordRichPresenceAction(0, "Discord", "your discord")
+        SetDiscordRichPresenceAction(1, "Website", "your website")
+
+		Citizen.Wait(5000)
+	end
+end)
